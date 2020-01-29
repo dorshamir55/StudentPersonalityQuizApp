d504443ecbf2e8d21cecea8f5fbdfa88ee6d1b40
elad <eladddd>
2020-01-29 23:45:03 +0200

    made question db class for the json file

---
+++ app/src/main/java/com/example/studentpersonalityquizapp/QuestionsDB.java
@@ -0,0 +1,34 @@
+package com.example.studentpersonalityquizapp;
+
+import java.io.Serializable;
+import java.util.Map;
+
+public class QuestionDB implements Serializable {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private Map<Integer,String> question;
+    private Map<Integer,Map<String,Integer>> answers;
+
+    public Question(Map<Integer, String> question, Map<Integer, Map<String, Integer>> answers) {
+        this.question = question;
+        this.answers = answers;
+    }
+
+    public Map<Integer, String> getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Map<Integer, String> question) {
+        this.question = question;
+    }
+
+    public Map<Integer, Map<String, Integer>> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(Map<Integer, Map<String, Integer>> answers) {
+        this.answers = answers;
+    }
+
