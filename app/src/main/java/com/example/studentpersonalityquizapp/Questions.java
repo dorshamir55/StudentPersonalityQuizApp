e50d20b3cf713efdcab4d140f8cc5ff222cebc7c
dor <dorshamir4@gmail.com>
2020-01-29 17:25:30 +0200

    KAKA

--- app/src/main/java/com/example/studentpersonalityquizapp/Questions.java
+++ app/src/main/java/com/example/studentpersonalityquizapp/Questions.java
@@ -1,23 +1,13 @@
 package com.example.studentpersonalityquizapp;
 
 import java.util.Map;
+import java.util.Set;
 
 public class Questions {
     private String question;
-    private String answer1;
-    private String answer2;
-    private String answer3;
-    private String answer4;
-    private int score;
-    private Map<String,Integer> answerMap;
 
-    public Questions(String question, String answer1, String answer2, String answer3, String answer4, int score, Map<String, Integer> answerMap) {
-        this.question = question;
-        this.answer1 = answer1;
-        this.answer2 = answer2;
-        this.answer3 = answer3;
-        this.answer4 = answer4;
-        this.score = score;
-        this.answerMap = answerMap;
+    private Map<Integer, Map<String,Integer>> answerMap;
+
+    public Questions(){
     }
 }
