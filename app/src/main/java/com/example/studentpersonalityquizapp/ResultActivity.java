20473767cc7ef316e863b30802088442ed750bd1
elad <eladddd>
2020-05-02 16:24:13 +0300

    some changes

--- app/src/main/java/com/example/studentpersonalityquizapp/ResultActivity.java
+++ app/src/main/java/com/example/studentpersonalityquizapp/ResultActivity.java
@@ -3,7 +3,6 @@ package com.example.studentpersonalityquizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
